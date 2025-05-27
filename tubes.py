fb31ab75eb8e8b9f280623779ebaf8384d7cab2f
ReynerCornelius <2472005@maranatha.ac.id>
2025-05-27 23:32:27 +0700

    commit

---
+++ tubes.py
@@ -0,0 +1,15 @@
+# File : Tubes.py 
+# Penulis : 2472010 2472012 2472005
+# Tujuan Program :
+# Kamus Data :
+
+def main():
+# Perintah Input
+
+# Perintah Proses
+    
+# Perintah Output
+    return 0
+
+if __name__ == '__main__':    
+    main()   
