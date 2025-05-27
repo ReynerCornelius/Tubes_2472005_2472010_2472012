9d6b094787f049420b1d9581ec1372155dbe21bb
JonathanVw10 <jojojonathan790@gmail.com>
2025-05-28 00:29:19 +0700

    third commit

--- tubes.py
+++ tubes.py
@@ -21,7 +21,7 @@ def peminjam(cek):
             print(f"Selamat datang {peminjam_nama[i]} - {peminjam_nrp[i]}")
         
 def daftar(cek):
-    print('test')
+    print('test 2')
 
 def main():
 # Perintah Input
