7cf6aea6dbac6e4fbbefca47f39d68074fc1e93e
Jason <2472012@maranatha.ac.id>
2025-05-28 01:16:19 +0700

    kedua

--- tubes.py
+++ tubes.py
@@ -4,6 +4,8 @@
 #
 # Kamus Data :
 #
+
+# menu peminjam,menu daftar peminjman,menu staff
 def peminjam(cek):
     kita = 3
     peminjam_nama = kita * [None]
@@ -18,8 +20,10 @@ def peminjam(cek):
     indeks = cari_peminjam(nrp)
     if nrp == peminjam_nrp[indeks]:
         print(f"Selamat datang {peminjam_nama[indeks]} - {peminjam_nrp[indeks]}")
+        # manggil fungsi untuk lanjutin ke menu peminjam
     else:
-        print("Nrp tidak terdaftar")
+        print("NRP tidak terdaftar")
+        # manggil fungsi untuk daftar
         return 0
         
 def cari_peminjam(nrp):
@@ -33,6 +37,17 @@ def cari_peminjam(nrp):
             return i
     return 0
 
+def staff(cek):
+    staff_code = 999999
+    kode = int(input("Masukkan kode staff :"))
+    if kode == staff_code:
+        print("Selamat datang Staff")
+        # manggil fungsi untuk lanjutin ke menu staff
+    else:
+        print("Kode staff salah")
+        return 0
+    
+
 # def daftar(cek):
 
 def main():
@@ -40,7 +55,8 @@ def main():
     cek = int(input("Apakah anda Exit/Peminjam/Staff (0,1,2) :"))
     if cek == 1:
         peminjam(cek)
-    # elif cek == 2:
+    elif cek == 2:
+        staff(cek)
 
         
 # Perintah Proses
