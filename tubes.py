23d6b7d2b0bde5afd9edf71409c787f3e7bdbc5d
Nakamaji <raymondedson09@gmail.com>
2025-05-30 11:59:53 +0700

    Progress Login

--- tubes.py
+++ tubes.py
@@ -3,31 +3,57 @@
 # Tujuan Program :
 #
 # Kamus Data :
-#
-def peminjam(cek):
-    kita = 3
-    peminjam_nama = kita * [None]
-    peminjam_nama[0] = "Jason"
-    peminjam_nama[1] = "Reyner"
-    peminjam_nama[2] = "Jonathan"
-    peminjam_nrp = kita * [None]
-    peminjam_nrp[0] = 2472012
-    peminjam_nrp[1] = 2472005
-    peminjam_nrp[2] = 2472010
-    nrp = int(input("Nrp :"))
-    for i in range (0,kita,1):
-        if peminjam_nrp[i] == nrp:
-            # jadi setelah memenuhi kondisi ia akan print selamat datang nama dan nrp tersebut
-            print(f"Selamat datang {peminjam_nama[i]} - {peminjam_nrp[i]}")
-        
-def daftar(cek):
-    print('test 2')
+import csv
 
+def login():
+    print("Silahkan masukan Nama dan NRP anda:")
+    print("==============================")
+    nama = input('Nama: ')
+    nrp = input("Nrp : ")
+    print("==============================")
+    with open('Data_peminjam.csv', mode='r', newline='')as f:
+        reader = csv.reader(f)    
+        for row in reader :
+            if (row[0] == nama and row[1] == nrp):
+                print('Hi')
+                menu()
+        else:
+            print()
+            daftar()
+            
+def menuPeminjam():
+    print("|                                |")
+    
+    
+    
+    
+    
+    
+    
+    
+def daftar():
+    print("NRP anda belum terdaftar!") 
+    print("Apakah anda ingin daftar?")
+    print("==============================")
+    nama_peminjam = str(input("Username: "))
+    nrp_peminjam = int(input("Nrp: "))
+    with open('Data_peminjam.csv', mode='a', newline='')as f:
+        writer = csv.writer(f)
+        writer.writerow([nama_peminjam, nrp_peminjam])
+    print("==============================")
+    print("Nama dan NRP anda sudah terdaftar")
+    print()
+    login()
+    
 def main():
 # Perintah Input
-    cek = int(input("Apakah anda Exit/Peminjam/Staff (0,1,2) :"))
+    print("Selamat Datang di Perpustakaan Maranatha!!")
+    print("Apakah anda ingin login?")
+    print("==============================")
+    cek = int(input("Ingin login sebagai apa? Exit/Peminjam/Staff (0,1,2) :"))
+    print("==============================")
     if cek == 1:
-        peminjam(cek)
+        login()
     # elif cek == 2:
 
         
